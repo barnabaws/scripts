a9868edc8610debf5fa97365162067919055e0f7
barnabaws <75345602+barnabaws@users.noreply.github.com>
2021-07-02 21:09:22 +0200

    Update funnywait.sh

--- funnywait.sh
+++ funnywait.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+# MIT License
+# 
+# Copyright (c) 2021 Mateusz Piwek
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+
 COMMAND=$2
 COMMENT=$1
 WAIT_ARG=$3
@@ -13,7 +36,7 @@ _FW_PID=$(pgrep $_FW_EXEC_NAME | head -n 1)
 
 if [ $# -eq 1 ] && [ $1 = 'status' ]; then 
 	if [ $_FW_PID -ne $_THISINSTANCE_PID ]; then
-		echo "$_FW_EXEC_NAME is running: " $_FW_PID
+		echo "$_FW_EXEC_NAME is running: "
 		echo $(pgrep -a $_FW_EXEC_NAME | head -n 1)
 	else
 		echo "No $_FW_EXEC_NAME instance"
